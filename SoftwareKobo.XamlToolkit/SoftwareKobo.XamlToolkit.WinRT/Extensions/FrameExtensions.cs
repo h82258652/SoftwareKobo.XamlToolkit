e4422dbafd8abaeb8719302a2c08458844d281f8
h82258652 <842053625@qq.com>
2015-01-17 16:42:32 +0800

    Init

---
+++ SoftwareKobo.XamlToolkit/SoftwareKobo.XamlToolkit.WinRT/Extensions/FrameExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace SoftwareKobo.XamlToolkit.Extensions
+{
+    public static class FrameExtensions
+    {
+        public static bool Navigate<TSourcePage>(this Frame frame) where TSourcePage : Page
+        {
+            return frame.Navigate(typeof(TSourcePage));
+        }
+
+        public static bool Navigate<TSourcePage>(this Frame frame, object parameter) where TSourcePage : Page
+        {
+            return frame.Navigate(typeof(TSourcePage), parameter);
+        }
+
+        public static bool Navigate<TSourcePage>(this Frame frame, object parameter, NavigationTransitionInfo infoOverride) where TSourcePage : Page
+        {
+            return frame.Navigate(typeof(TSourcePage), parameter, infoOverride);
+        }
+    }
+}
