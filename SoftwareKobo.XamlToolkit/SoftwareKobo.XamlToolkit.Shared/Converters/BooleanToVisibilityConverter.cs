5f16c2bda31dca688ab3751ce10690efcf42ee4a
h82258652 <842053625@qq.com>
2015-01-17 17:03:02 +0800

    update

---
+++ SoftwareKobo.XamlToolkit/SoftwareKobo.XamlToolkit.Shared/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,38 @@
+﻿#if WINDOWS_APP || WINDOWS_PHONE_APP
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace SoftwareKobo.XamlToolkit.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool bValue = false;
+            if (value is bool)
+            {
+                bValue = (bool)value;
+            }
+            else if (value is bool?)
+            {
+                bool? tmp = (bool?)value;
+                bValue = tmp.HasValue ? tmp.Value : false;
+            }
+            return (bValue) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Visibility)
+            {
+                return (Visibility)value == Visibility.Visible;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+#endif
---
+++ SoftwareKobo.XamlToolkit/SoftwareKobo.XamlToolkit.Shared/Converters/VisibilityToBooleanConverter.cs
@@ -0,0 +1,59 @@
+﻿#if !WINDOWS_FORM_APP
+using System;
+#if WPF||SILVERLIGHT
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+#endif
+#if WinRT
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+#endif
+
+namespace SoftwareKobo.XamlToolkit.Converters
+{
+    public class VisibilityToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+#if WPF||SILVERLIGHT
+            CultureInfo culture
+#endif
+#if WinRT
+            string language
+#endif
+            )
+        {
+            if (value is Visibility)
+            {
+                return (Visibility)value == Visibility.Visible;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+#if WPF||SILVERLIGHT
+            CultureInfo culture
+#endif
+#if WinRT
+            string language
+#endif
+            )
+        {
+            bool bValue = false;
+            if (value is bool)
+            {
+                bValue = (bool)value;
+            }
+            else if (value is bool?)
+            {
+                bool? tmp = (bool?)value;
+                bValue = tmp.HasValue ? tmp.Value : false;
+            }
+            return (bValue) ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
+#endif
